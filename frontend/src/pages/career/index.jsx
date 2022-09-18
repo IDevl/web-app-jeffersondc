7b1b2dd13a61584c05b10a937598490f765fd589
lDevl <jbdelacruz411@gmail.com>
2022-09-18 15:46:48 +0800

    a

--- frontend/src/pages/career/index.jsx
+++ frontend/src/pages/career/index.jsx
@@ -196,7 +196,7 @@ const Career = props => {
 				</Container>
 
 				<Container className="container" my="xl">
-					{!careers && <Loader color="dark" variant="dots" />}
+					{!careers && <Loader color="dark" variant="dots" size="xl"/>}
 
 					{careers && <Grid mt="sm" columns={3} justify="start">
 						{careers.map(careers => {
